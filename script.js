4d95d8852330e3b6ef5f19451c6e888feca905a4
notmeksi <its.royalekelly@gmail.com>
2026-02-21 10:27:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("This is the Rock Paper Scissors Game");
+
+function getComputerChoice() {
+    if (Math.random() < 0.34) {
+        return "rock";
+    } else if (Math.random() <= 0.67) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let input = prompt("Do you choose rock, paper or scissors?");
+    return input.toLowerCase();
+}
+
+const humanScore = 0;
+const computerScore = 0;
+
+
+function playRound(ComputerChoice, humanChoice) {
+    if (ComputerChoice === humanChoice) {
+        return "It's a tie!";
+    } else if (ComputerChoice === "rock" && humanChoice === "paper") {
+        humanScore++;
+        return "You win! Paper beats rock";
+    } else if (ComputerChoice === "paper" && humanChoice === "scissors") {
+        humanScore++;
+        return "You win! Scissors beats paper";
+    } else if (ComputerChoice === "scissors" && humanChoice === "rock") {
+        humanScore++;
+        return "You win! Rock beats scissors";
+    } else if (ComputerChoice === "rock" && humanChoice === "scissors") {
+        computerScore++;
+        return "Oh no! You lose! Rock beats scissors";
+    } else if (ComputerChoice === "paper" && humanChoice === "rock") {
+        computerScore++;
+        return "Oh no! You lose! Paper beats rock";
+    } else if (ComputerChoice === "scissors" && humanChoice === "paper") {
+        computerScore++;
+        return "Oh no! You lose! Scissors beats paper";
+    } else {
+        return "Invalid input ! Please choose rock, paper or scissors.";
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(computerSelection, humanSelection));
